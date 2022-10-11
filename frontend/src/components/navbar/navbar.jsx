3253bb7f7c8d4d2d883b69e290250dd2899012da
FredrikFalto <faltofredrik@gmail.com>
2022-10-11 10:10:38 +0200

    Added icons and built finished the navbar for mobile users

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 
-import logo from '../../img/Logo.svg';
+import './navbar.css';
+
+// import logo from '../../img/Logo.svg';
+import ProductIcon from '../../img/ProdukterIcon.svg';
+import PlusIcon from '../../img/PlusIcon.svg';
+import MessagesIcon from '../../img/MessagesIcon.svg';
+import ProfileIcon from '../../img/ProfileIcon.svg';
 
 const Navbar = () => {
     return (
-        <nav className="bg-white border-gray-900 px-2 w-full fixed sm:bottom-0 md:top-0">
-            <ul className="flex flex-row p-4 mt-4 bg-gray-50 border items-stretch">
+        <nav className="bg-white border border-gray-300 px-2 w-full fixed bottom-0 md:top-0">
+            <ul className="flex flex-row my-1 justify-center">
                 <li>
-                    <a href="#" className="m-5">
+                    <a href="/home" className="m-1 flex flex-col">
+                        <img src={ProductIcon} alt="" id="navIcon" />
                         Annonser
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="m-5">
+                    <a href="/home" className="m-1 flex flex-col">
+                        <img src={PlusIcon} alt="" id="navIcon" />
                         Ny annons
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="m-5">
+                    <a href="/home" className="m-1 flex flex-col">
+                        <img src={MessagesIcon} alt="" id="navIcon" />
                         Meddelanden
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="m-5">
+                    <a href="/home" className="m-1 flex flex-col">
+                        <img src={ProfileIcon} alt="" id="navIcon" />
                         Min Profil
                     </a>
                 </li>
