e0fc4aa8755d3c84f967dd25c623ee26ee39e264
FredrikFalto <faltofredrik@gmail.com>
2022-10-14 13:17:24 +0200

    Tidied up some backend code

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,1 +1,50 @@
-const Workout = require('../models/recipeModel');
+const Recipe = require('../models/recipeModel');
+const mongoose = require('mongoose');
+
+// Get all recipes
+const getRecipes = async (req, res) => {
+    const recipes = await Recipe.find({}).sort({});
+
+    res.status(200).json(recipes);
+};
+
+// Get a single recipe
+const getRecipe = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such recipe' });
+    }
+
+    const recipe = await Recipe.findById(id);
+
+    if (!recipe) {
+        return res.status(404).json({ error: 'No such recipe' });
+    }
+
+    res.status(200).json(recipe);
+};
+
+// Create new recipe
+const createRecipe = async (req, res) => {
+    const { dish, ingredients, allergens, category, link } = req.body;
+
+    try {
+        const recipe = await Recipe.create({
+            dish,
+            ingredients,
+            allergens,
+            category,
+            link,
+        });
+        res.status(200).json(recipe);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    createRecipe,
+    getRecipe,
+    getRecipes,
+};
--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,46 +1,19 @@
 const express = require('express');
-const Recipe = require('../models/recipeModel');
+const {
+    createRecipe,
+    getRecipe,
+    getRecipes,
+} = require('../controllers/recipeController');
 
 const router = express.Router();
 
 // Hämta alla recept
-router.get('/', (req, res) => {
-    res.json({ mssg: 'GET all recipes' });
-});
+router.get('/', getRecipes);
 
 // Hämta ett recept
-router.get('/:id', (req, res) => {
-    res.json({ mssg: 'GET a single recipe' });
-});
+router.get('/:id', getRecipe);
 
 // Lägga till ett recept
-router.post('/', async (req, res) => {
-    const { dish, ingredients, allergens, category, link } = req.body;
-
-    try {
-        const recipe = await Recipe.create({
-            dish,
-            ingredients,
-            allergens,
-            category,
-            link,
-        });
-        res.status(200).json(recipe);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+router.post('/', createRecipe);
 
 module.exports = router;
-
-// !! Används ej, sparat utifall att det behövs senare !!
-
-// // Ta bort ett recept
-// router.delete('/:id', (req, res) => {
-//     res.json({ mssg: 'DELETE a recipe' });
-// });
-
-// // Uppdatera ett recept
-// router.patch('/:id', (req, res) => {
-//     res.json({ mssg: 'UPDATE a recipe' });
-// });
