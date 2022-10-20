2cf3be4a3949b9fec239bbfa67e8d0c392e9ef73
FredrikFalto <faltofredrik@gmail.com>
2022-10-20 17:38:31 +0200

    Started working on the categories bar

--- frontend/src/pages/listings/listings.jsx
+++ frontend/src/pages/listings/listings.jsx
@@ -8,10 +8,33 @@ const Listings = () => {
         <div>
             <Searchbar />
             <Navbar />
-
             <hr class="my-1 h-px bg-gray-200 border-0 shadow" />
 
             <h3 className="ml-2 text-lg">Vad är du sugen på?</h3>
+
+            <div className="flex flex-row gap-1 overflow-x-auto m-2">
+                <div className="card">
+                    <h1>Welcome</h1>
+                </div>
+                <div className="card">
+                    <h1>Welcome</h1>
+                </div>
+                <div className="card">
+                    <h1>Welcome</h1>
+                </div>
+                <div className="card">
+                    <h1>Welcome</h1>
+                </div>
+                <div className="card">
+                    <h1>Welcome</h1>
+                </div>
+                <div className="card">
+                    <h1>Welcome</h1>
+                </div>
+                <div className="card">
+                    <h1>Welcome</h1>
+                </div>
+            </div>
         </div>
     );
 };
