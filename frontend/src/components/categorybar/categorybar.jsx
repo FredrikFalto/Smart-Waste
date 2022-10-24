6e0912db3f42e72c6e63ce4a20abe9608a43d55c
FredrikFalto <faltofredrik@gmail.com>
2022-10-24 13:55:12 +0200

    Built categorybar as a component

---
+++ frontend/src/components/categorybar/categorybar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Meat from '../../img/Mobile/CategoryMeatMobile.svg';
+import Vegetarian from '../../img/Mobile/CategoryVegetarianMobile.svg';
+import Fish from '../../img/Mobile/CategoryFishMobile.svg';
+import Chicken from '../../img/Mobile/CategoryChickenMobile.svg';
+
+const Categorybar = () => {
+    return (
+        <div className="flex flex-row overflow-x-auto mx-1 gap-1 category-card-row">
+            <div>
+                <a href="#">
+                    <img src={Meat} alt="Kött" className="" />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src={Vegetarian} alt="Vegetariskt" className="" />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src={Fish} alt="Fisk" className="" />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src={Chicken} alt="Kyckling" className="" />
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Categorybar;
--- frontend/src/pages/listings/listings.jsx
+++ frontend/src/pages/listings/listings.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 
-import './listings.css';
-
-import Meat from '../../img/Mobile/CategoryMeatMobile.svg';
-import Vegetarian from '../../img/Mobile/CategoryVegetarianMobile.svg';
-import Fish from '../../img/Mobile/CategoryFishMobile.svg';
-import Chicken from '../../img/Mobile/CategoryChickenMobile.svg';
-
 import Navbar from '../../components/navbar/navbar';
 import Searchbar from '../../components/searchbar/searchbar';
+import Categorybar from '../../components/categorybar/categorybar';
 
 const Listings = () => {
     return (
@@ -18,35 +12,9 @@ const Listings = () => {
             <hr class="my-1 h-px bg-gray-200 border-0 shadow" />
 
             <h3 className="ml-2 text-lg">Vad är du sugen på?</h3>
+            <Categorybar />
 
-            <div className="flex flex-row overflow-x-auto mx-1 gap-1">
-                <a href="">
-                    <img src={Meat} alt="Kött" className="card card-item p-0" />
-                </a>
-                <a href="">
-                    <img
-                        src={Vegetarian}
-                        alt="Vegetariskt"
-                        className="card p-0"
-                    />
-                </a>
-                <a href="">
-                    <img src={Fish} alt="Fisk" className="card p-0" />
-                </a>
-                <a href="">
-                    <img src={Chicken} alt="Kyckling" className="card p-0" />
-                </a>
-                <a href="">
-                    <img src={Chicken} alt="Kyckling" className="card p-0" />
-                </a>
-            </div>
-
-            <div className="flex flex-row overflow-x-auto mx-1 gap-1 category-card-row">
-                <img src={Meat} alt="Kött" className="card p-0" />
-                <img src={Vegetarian} alt="Vegetariskt" className="card p-0" />
-                <img src={Fish} alt="Fisk" className="card p-0" />
-                <img src={Chicken} alt="Kyckling" className="card p-0" />
-            </div>
+            <h3 className="ml-2 text-xl pt-6">Trött på det gamla vanliga?</h3>
         </div>
     );
 };
