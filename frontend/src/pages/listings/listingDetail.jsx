331d191bfb395dc0a8837fc894fe1e21d674f037
FredrikFalto <faltofredrik@gmail.com>
2022-10-27 16:11:28 +0200

    Changes to the detailed listings page

--- frontend/src/pages/listings/listingDetail.jsx
+++ frontend/src/pages/listings/listingDetail.jsx
@@ -23,15 +23,39 @@ const Listing = () => {
                 const listingDetail = () => {
                     return (
                         <div className="grid">
-                            <img src={listing.imglink} alt={listing.dish} />
-                            <p>
-                                Bäst före:
-                                {format(
-                                    new Date(listing.expirationdate),
-                                    ' dd/MM'
-                                )}
-                            </p>
-                            <h1 className="text-xl">{listing.dish}</h1>
+                            <img
+                                src={listing.imglink}
+                                alt={listing.dish}
+                                className="w-full"
+                            />
+                            <div className="p-2">
+                                <p className="pb-1">
+                                    Bäst före:
+                                    {format(
+                                        new Date(listing.expirationdate),
+                                        ' yyyy-MM-dd'
+                                    )}
+                                </p>
+                                <h3 className="text-2xl">{listing.dish}</h3>
+                                <h3 className="text-2xl font-bold">
+                                    {listing.price}kr
+                                </h3>
+
+                                <hr className="my-1 h-px bg-gray-300 border-0 shadow" />
+
+                                <p className="font-medium">Allergener:</p>
+                                <p> {listing.allergens.join(', ')}</p>
+
+                                <hr className="my-1 h-px bg-gray-300 border-0 shadow" />
+
+                                <p className="font-medium">Ingredienser:</p>
+                                <p> {listing.ingredients.join(', ')}</p>
+
+                                <hr className="my-1 h-px bg-gray-300 border-0 shadow" />
+
+                                <p className="font-medium">Extra info:</p>
+                                <p> {listing.extrainfo}</p>
+                            </div>
                         </div>
                     );
                 };
@@ -45,7 +69,7 @@ const Listing = () => {
     return (
         <div>
             <Navbar />
-            <div>{item}</div>
+            <div className="container mx-auto">{item}</div>
         </div>
     );
 };
