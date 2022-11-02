65ec1cf3f461e0b6f686b7977c12d868185038c7
FredrikFalto <faltofredrik@gmail.com>
2022-11-02 14:48:00 +0100

    Created hooks and context for user signup and login

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -5,71 +5,65 @@ import Logo from '../../img/Logo.svg';
 
 const LogIn = () => {
     return (
-        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="w-full max-w-md space-y-8">
+        <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+            <div className='w-full max-w-md space-y-8'>
                 <div>
                     <img
-                        className="mx-auto"
+                        className='mx-auto'
                         src={Logo}
-                        alt="Smart Waste logo"
+                        alt='Smart Waste logo'
                     />
-                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
                         Logga in
                     </h2>
                 </div>
-                <form className="mt-8 space-y-6" action="#" method="POST">
-                    <input type="hidden" name="remember" value="true" />
-                    <div className="-space-y-px rounded-md shadow-sm">
+                <form className='mt-8 space-y-6' action='#' method='POST'>
+                    <input type='hidden' name='remember' value='true' />
+                    <div className='-space-y-px rounded-md shadow-sm'>
                         <div>
-                            <label htmlFor="email-address" className="sr-only">
+                            <label htmlFor='email-address' className='sr-only'>
                                 Email address
                             </label>
                             <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
+                                id='email-address'
+                                name='email'
+                                type='email'
+                                autoComplete='email'
                                 required
-                                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-green-600 sm:text-sm"
-                                placeholder="Email"
+                                className='relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-green-600 sm:text-sm'
+                                placeholder='Email'
                             />
                         </div>
                         <div>
-                            <label htmlFor="password" className="sr-only">
+                            <label htmlFor='password' className='sr-only'>
                                 Password
                             </label>
                             <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                autoComplete="current-password"
+                                id='password'
+                                name='password'
+                                type='password'
+                                autoComplete='current-password'
                                 required
-                                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                placeholder="Lösenord"
+                                className='relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                                placeholder='Lösenord'
                             />
                         </div>
                     </div>
 
-                    <div className="flex items-center justify-between">
-                        <div className="flex items-center">
-                            <input
-                                id="remember-me"
-                                name="remember-me"
-                                type="checkbox"
-                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                            <label
-                                htmlFor="remember-me"
-                                className="ml-2 block text-sm text-gray-900"
+                    <div className='flex items-center justify-between'>
+                        <div className='text-sm'>
+                            <a
+                                href='/'
+                                className='font-medium text-indigo-600 hover:text-indigo-500'
                             >
-                                Kom ihåg mig
-                            </label>
+                                Skapa konto
+                            </a>
                         </div>
 
-                        <div className="text-sm">
+                        <div className='text-sm'>
                             <a
-                                href="/"
-                                className="font-medium text-indigo-600 hover:text-indigo-500"
+                                href='/'
+                                className='font-medium text-indigo-600 hover:text-indigo-500'
                             >
                                 Glömt lösenord?
                             </a>
@@ -77,16 +71,16 @@ const LogIn = () => {
                     </div>
 
                     <div>
-                        <Link to="/home">
+                        <Link to='/home'>
                             <button
-                                type="submit"
-                                className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white"
+                                type='submit'
+                                className='group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white'
                             >
-                                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                                <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
                                     <path
-                                        fillRule="evenodd"
-                                        d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                                        clipRule="evenodd"
+                                        fillRule='evenodd'
+                                        d='M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z'
+                                        clipRule='evenodd'
                                     />
                                 </span>
                                 Logga in
@@ -96,7 +90,7 @@ const LogIn = () => {
                 </form>
             </div>
         </div>
-    );
+    )
 };
 
 export default LogIn;
