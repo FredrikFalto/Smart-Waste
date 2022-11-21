df3d93516f42328578a0fbdc96a73f2c9737e263
FredrikFalto <faltofredrik@gmail.com>
2022-11-21 16:35:01 +0100

    Minor changes

--- frontend/src/pages/listings/newListing.jsx
+++ frontend/src/pages/listings/newListing.jsx
@@ -4,12 +4,22 @@ import Navbar from '../../components/navbar/navbar'
 import ListingForm from '../../components/listing/ListingForm'
 
 const newListing = () => {
+    const loggedUser = JSON.parse(localStorage.getItem('user'))
+
     return (
         <div>
             <Navbar />
 
             <div className='container mx-auto px-4 pb-20'>
-                <ListingForm />
+                {loggedUser && <ListingForm />}
+
+                {!loggedUser && 
+                    <div className='mt-96'>
+                        <h1 className='text-xl w-full text-center'>
+                            Du måste logga in för att skapa annons
+                        </h1>
+                    </div>
+                }
             </div>
         </div>
     )
