7c1bb0da73e457ef5475a1349e9093feb00d4249
FredrikFalto <faltofredrik@gmail.com>
2022-11-23 20:17:36 +0100

    more responsiveness

--- frontend/src/pages/listings/successfulListing.jsx
+++ frontend/src/pages/listings/successfulListing.jsx
@@ -21,18 +21,18 @@ const successfulListing = () => {
                 <img
                     src={SuccessIcon}
                     alt='Success icon'
-                    className='mt-40 w-1/3'
+                    className='mt-40 w-1/3 md:w-2/6 lg:w-1/4 xl:w-1/5'
                 />
                 <p className='text-2xl pt-6'>Tack för din annons!</p>
                 <button
                     onClick={myListings}
-                    className='w-full rounded-md border border-transparent bg-green-600 py-2 px-4 mt-5 text-sm font-medium text-white'
+                    className='w-full rounded-md border border-transparent bg-green-600 py-2 px-4 mt-5 text-sm font-medium text-white md:w-4/5 lg:w-3/5 xl:w-2/6'
                 >
                     Visa mina annonser
                 </button>
                 <button
                     onClick={newListing}
-                    className='w-full rounded-md border border-black py-2 px-4 mt-3 text-sm font-medium text-black'
+                    className='w-full rounded-md border border-black py-2 px-4 mt-3 text-sm font-medium text-black md:w-4/5 lg:w-3/5 xl:w-2/6'
                 >
                     Ny annons
                 </button>
